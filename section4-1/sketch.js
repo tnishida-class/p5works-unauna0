3eb43bc1255883ce7e68f9bbb9ce84ee97f8fd7c
unauna0 <031420.yuu@gmail.com>
2020-11-23 22:04:43 +0900

    update 3-q1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -9,8 +9,8 @@ function windowResized(){
 
 function draw(){
   background(160, 192, 255);
-  for(let i = 0; i < 10; i++){
-    ellipse(width * i / 10, height * (10 - i) / 10, 20);
+  for(let i = 0; i < 5; i++){
+    ellipse(width * i / 10, height * (10 - 1) / 10, 20);
   }
-  text("center!", width * 0.5, height * 0.5);
+  text("center!", width * 0.1, height * 0.5);
 }
