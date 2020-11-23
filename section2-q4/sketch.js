4e3495d991899ae1a88b1fbe5a6a062000ca4adf
unauna0 <031420.yuu@gmail.com>
2020-11-24 01:17:03 +0900

    update 2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,24 @@
-// ギリシャ国旗
-function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
-  // noStroke();
-  background(255);
 
-  let d = height / 9; // 縞1本の太さ
 
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+function setup(){
+  createCanvas(200, 200);
+  background(192);
+for(let i = 0; i < 12; i++){
+    let theta = TWO_PI * i / 12;
+    let x = 100 + cos(theta) * 50;
+    let y = 100 + sin(theta) * 50;
+    ellipse(x, y, 10);
+    star(x, y, 10)
   }
-
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
-
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
+
+  function star(cx, cy, r){
+    beginShape();    // 点つなぎを始める
+    for(let i = 0; i < 5; i++){
+      const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+      const x = cx + cos(theta) * r;
+      const y = cy + sin(theta) * r;
+      vertex(x, y);  // 次につなぐ点を１つ増やす
+    }
+    endShape(CLOSE); // 点つなぎを終わる
+  }
